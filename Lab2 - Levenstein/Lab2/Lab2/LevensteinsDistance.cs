b5f4f2237be612573e10eac7caaab00189f8f090
Borizzler <116744797+1ITManiac1@users.noreply.github.com>
2023-09-27 10:23:08 +0300

    DEbugged

--- Lab2 - Levenstein/Lab2/Lab2/LevensteinsDistance.cs
+++ Lab2 - Levenstein/Lab2/Lab2/LevensteinsDistance.cs
@@ -17,9 +17,8 @@ namespace LevensteinApp
                 foreach (var el in Inp) if (char.IsLetter(el)) count++;
             }
             //Having taken in a string with letters, delete everything non-letter
-            StringBuilder Result = new StringBuilder('a'*count);
-            int curr_index = 0;
-            foreach (var x in Inp) if (char.IsLetter(x)) { Result[curr_index] = x; curr_index++; };
+            StringBuilder Result = new StringBuilder(count);
+            foreach (var x in Inp) if (char.IsLetter(x)) { Result.Append(x);};
             return Result.ToString();
         }
 
@@ -34,37 +33,32 @@ namespace LevensteinApp
             string Word1 = GetInput("Enter the first word: ");
             string Word2 = GetInput("Enter the second word: ");
 
-            Console.WriteLine(Word1 + " " + Word2);
-
-            int[,] VF_Matrix = new int[Word1.Length + 1, Word2.Length + 1]; //Creating a Vagner-Fischer`s Matrix
+            int[,] VF_Matrix = new int[Word2.Length + 1, Word1.Length + 1]; //Creating a Vagner-Fischer`s Matrix
 
             //Initializing the first line of the Matrix
-            for (int i = 0; i < Word2.Length + 1; ++i)
+            for (int i = 0; i < Word1.Length + 1; ++i)
             {
                 VF_Matrix[0, i] = i;
             }
 
             //Initializing the first column of the Matrix
-            for (int j = 0; j < Word1.Length + 1; ++j)
+            for (int j = 0; j < Word2.Length + 1; ++j)
             {
                 VF_Matrix[j, 0] = j;
             }
 
             //Filling the matrix up using VF method
-            for (int column = 1; column < VF_Matrix.Rank(0); ++column)
+            for (int line = 1; line < VF_Matrix.GetLength(0); ++line)
             {
-                for (int line = 1; line < VF_Matrix.Rank[1]; ++line)
+                for (int column = 1; column < VF_Matrix.GetLength(1); ++column)
                 {
                     //Whether we +1 the left-up num in the comparison depends on whether the letters of the current pair are equal
-                    VF_Matrix[line, column] = max(VF_Matrix[line - 1, column] + 1, VF_Matrix[line, column - 1] + 1, VF_Matrix[line - 1, column - 1] + 1 * (VF_Matrix[0, column] != VF_Matrix[line, 0]));
+                    VF_Matrix[line, column] = Math.Min(Math.Min(VF_Matrix[line - 1, column] + 1, VF_Matrix[line, column - 1] + 1), VF_Matrix[line - 1, column - 1] + Convert.ToInt32(Word1[column-1] != Word2[line-1]));
                 }
             }
-            //Got the Matrix, now the most faraway from (0,0) cell contains the Levenstein`s Distance.
-            Console.WriteLine("The Levenstein`s Distance for the input pair of words = {0}", VF_Matrix[Word1.Length + 1, Word2.Length + 1]);
 
-            //To trace the path of permutations, we`ll use a recurrent function
-            Run_It_Back(0, 0, Word1, Word2, VF_Matrix);
-
+            //Printing the matrix to check
+            Console.WriteLine("\nVagner-Fischer`s Matrix:\n");
             for (int i = 0; i < VF_Matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < VF_Matrix.GetLength(1); j++)
@@ -73,6 +67,12 @@ namespace LevensteinApp
                 }
                 Console.WriteLine();
             }
+
+            //Got the Matrix, now the most faraway from (0,0) cell contains the Levenstein`s Distance.
+            Console.WriteLine("\nThe Levenstein`s Distance for the input pair of words = {0}", VF_Matrix[Word2.Length, Word1.Length]);
+
+            //To trace the path of permutations, we`ll use a recurrent function
+            Run_It_Back(0, 0, Word1, Word2, VF_Matrix);
 
             Console.Write("\nProgram finished. Press any key to terminate . . .");
             Console.ReadKey();
