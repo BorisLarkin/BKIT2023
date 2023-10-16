b204857ca7ca4456c1273be5e16b0175f26b2af4
ITManiac <116744797+1ITManiac1@users.noreply.github.com>
2023-10-16 23:50:02 +0300

    Update field.py

--- Lab3 Functional Py/field.py
+++ Lab3 Functional Py/field.py
@@ -1,12 +1,12 @@
-# Пример:
-# goods = [
-#    {'title': 'Ковер', 'price': 2000, 'color': 'green'},
-#    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'}
-# ]
-# field(goods, 'title') должен выдавать 'Ковер', 'Диван для отдыха'
-# field(goods, 'title', 'price') должен выдавать {'title': 'Ковер', 'price': 2000}, {'title': 'Диван для отдыха', 'price': 5300}
-
 def field(items, *args):
     assert len(args) > 0, 'Empty parameter sweep'
-    return []
-    
+    result = []
+    for item in items:
+        if len(args)==1:
+            result.append(item[arg])
+        else:
+            line = {}
+            for arg in args:
+                line[arg] = item [arg]
+            result.append(line)
+    return result
