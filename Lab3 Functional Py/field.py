dfa70ed961fd7b800e04adf3e7db0d6f98e80fb5
ITManiac <116744797+1ITManiac1@users.noreply.github.com>
2023-10-16 20:53:45 +0300

    CreatingFiles

---
+++ Lab3 Functional Py/field.py
@@ -0,0 +1,12 @@
+# Пример:
+# goods = [
+#    {'title': 'Ковер', 'price': 2000, 'color': 'green'},
+#    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'}
+# ]
+# field(goods, 'title') должен выдавать 'Ковер', 'Диван для отдыха'
+# field(goods, 'title', 'price') должен выдавать {'title': 'Ковер', 'price': 2000}, {'title': 'Диван для отдыха', 'price': 5300}
+
+def field(items, *args):
+    assert len(args) > 0, 'Empty parameter sweep'
+    return []
+    
