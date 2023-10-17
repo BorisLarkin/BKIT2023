28aae06bded0e73a3c69bb77da72cece6be97809
Borizzler <116744797+1ITManiac1@users.noreply.github.com>
2023-10-17 11:20:12 +0300

    Gen_random

--- Lab3 Functional Py/field.py
+++ Lab3 Functional Py/field.py
@@ -1,23 +1,21 @@
 def field(items, *args):
     assert len(args) > 0, 'Empty parameter sweep'
-    result = []
     for item in items:
         if len(args)==1:
-            result.append(item[args[0]])
+            yield item[args[0]]
         else:
             line = {}
             for arg in args:
                 if arg in item:
                     line[arg] = item[arg]
-            result.append(line)
-    return result
+            yield line
 
 def main():
     goods = [
     {'title': 'Ковер', 'price': 2000, 'color': 'green'},
     {'title': 'Диван для отдыха', 'color': 'black'}
 ]
-    print(field(goods,'title', 'price'))
+    print(list(field(goods,'title', 'price')))
     return 
 
 if __name__ == "__main__":
---
+++ Lab3 Functional Py/gen_random.py
@@ -0,0 +1,12 @@
+from random import randint
+def gen_random(num_count, begin, end):
+    assert num_count != 0, 'Empty generator'
+    for iteration in range(num_count):
+        yield randint(begin,end)
+
+def main():
+    print(list(gen_random(5,1,3)))
+    return 
+
+if __name__ == "__main__":
+    main()
