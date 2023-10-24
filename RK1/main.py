4958d6ba65fb32233efe486bce1dbd2f2e3f8932
Borizzler <116744797+1ITManiac1@users.noreply.github.com>
2023-10-25 00:40:03 +0300

    Update main.py

--- RK1/main.py
+++ RK1/main.py
@@ -4,10 +4,12 @@ from operator import itemgetter
 #Язык программирования - средство разработки
 class Lang:
     """ЯП"""
-    def __init__(self, id, name, ver):
+    def __init__(self, id, name, ver, share, ide_id):
         self.id = id
         self.ver = ver
         self.name = name
+        self.market_share = share
+        self.ide_id = ide_id
 
 
 class IDE:
@@ -30,39 +32,39 @@ class Lang_IDE:
 
 # Languages
 langs = [
-    Lang(1, 'Python', '3.11'),
-    Lang(2, 'C#', '11'),
-    Lang(3, 'C', 'C17'),
+    Lang(1, 'Python', '3.11', 29.48, 2),
+    Lang(2, 'C#', '11', 6.94,1),
+    Lang(3, 'C', 'C17', 6.49,2),
 
-    Lang(11, 'C++', 'C++20'),
-    Lang(22, 'JavaScript', ),
-    Lang(33, ''),
+    Lang(11, 'C++', 'C++20', 6.49,4),
+    Lang(22, 'Java', 'Java SE 21', 17.18,5),
+    Lang(33, 'GO', '1.21.3', 36.1,3),
 ]
 
 
 # Сотрудники
-emps = [
-    Emp(1, 'Артамонов', 25000, 1),
-    Emp(2, 'Петров', 35000, 2),
-    Emp(3, 'Иваненко', 45000, 3),
-    Emp(4, 'Иванов', 35000, 3),
-    Emp(5, 'Иванин', 25000, 3),
+IDEs = [
+    IDE(1, 'Microsoft Visual Studio', 2023),
+    IDE(2, 'Visual Studio Code', 2023),
+    IDE(3, 'Komodo', 2022),
+    IDE(4, 'IntelliJ IDEA', 2023),
+    IDE(5, 'Eclipse', 2022),
 ]
 
 
-emps_deps = [
-    EmpDep(1,1),
-    EmpDep(2,2),
-    EmpDep(3,3),
-    EmpDep(3,4),
-    EmpDep(3,5),
+IDEs_langs = [
+    Lang_IDE(1,1),
+    Lang_IDE(2,2),
+    Lang_IDE(3,3),
+    Lang_IDE(3,4),
+    Lang_IDE(3,5),
 
 
-    EmpDep(11,1),
-    EmpDep(22,2),
-    EmpDep(33,3),
-    EmpDep(33,4),
-    EmpDep(33,5),
+    Lang_IDE(11,1),
+    Lang_IDE(22,2),
+    Lang_IDE(33,3),
+    Lang_IDE(33,4),
+    Lang_IDE(33,5),
 ]
 
 
@@ -71,20 +73,20 @@ def main():
 
 
     # Соединение данных один-ко-многим 
-    one_to_many = [(e.fio, e.sal, d.name) 
-        for d in deps 
-        for e in emps 
-        if e.dep_id==d.id]
+    one_to_many = [(l.name, l.ver, i.name) 
+        for l in langs 
+        for i in IDEs
+        if i.id==l.ide_id]
     
     # Соединение данных многие-ко-многим
-    many_to_many_temp = [(d.name, ed.dep_id, ed.emp_id) 
-        for d in deps 
-        for ed in emps_deps 
+    many_to_many_temp = [(d.name, ed.dep_id, ed.IDE_id) 
+        for l in langs 
+        for ed in IDEs_langs 
         if d.id==ed.dep_id]
     
     many_to_many = [(e.fio, e.sal, dep_name) 
-        for dep_name, dep_id, emp_id in many_to_many_temp
-        for e in emps if e.id==emp_id]
+        for dep_name, dep_id, IDE_id in many_to_many_temp
+        for e in IDEs if e.id==IDE_id]
 
 
     print('Задание А1')
@@ -96,11 +98,11 @@ def main():
     # Перебираем все отделы
     for d in deps:
         # Список сотрудников отдела
-        d_emps = list(filter(lambda i: i[2]==d.name, one_to_many))
+        d_IDEs = list(filter(lambda i: i[2]==d.name, one_to_many))
         # Если отдел не пустой        
-        if len(d_emps) > 0:
+        if len(d_IDEs) > 0:
             # Зарплаты сотрудников отдела
-            d_sals = [sal for _,sal,_ in d_emps]
+            d_sals = [sal for _,sal,_ in d_IDEs]
             # Суммарная зарплата сотрудников отдела
             d_sals_sum = sum(d_sals)
             res_12_unsorted.append((d.name, d_sals_sum))
@@ -117,12 +119,12 @@ def main():
     for d in deps:
         if 'отдел' in d.name:
             # Список сотрудников отдела
-            d_emps = list(filter(lambda i: i[2]==d.name, many_to_many))
+            d_IDEs = list(filter(lambda i: i[2]==d.name, many_to_many))
             # Только ФИО сотрудников
-            d_emps_names = [x for x,_,_ in d_emps]
+            d_IDEs_names = [x for x,_,_ in d_IDEs]
             # Добавляем результат в словарь
             # ключ - отдел, значение - список фамилий
-            res_13[d.name] = d_emps_names
+            res_13[d.name] = d_IDEs_names
 
 
     print(res_13)
