cec2e86b804baac0e8dea2a7efd51c83a07f0cf8
ITManiac <borislarkin18@mail.ru>
2023-12-27 22:55:42 +0300

    RK2 done

--- RK2/main.py
+++ RK2/main.py
@@ -1,10 +1,12 @@
 # используется для сортировки
 from operator import itemgetter
 
-#Язык программирования - средство разработки
+
+# Язык программирования - средство разработки
 class Lang:
     """ЯП"""
     """share = market share, %"""
+
     def __init__(self, id, name, ver, share, ide_id):
         self.id = id
         self.ver = ver
@@ -15,6 +17,7 @@ class Lang:
 
 class IDE:
     """Средство разработки"""
+
     def __init__(self, id, name, year):
         self.id = id
         self.name = name
@@ -26,6 +29,7 @@ class Lang_IDE:
     'Языки, поддерживаемые IDE' для реализации 
     связи многие-ко-многим
     """
+
     def __init__(self, l_id, ide_id):
         self.l_id = l_id
         self.ide_id = ide_id
@@ -33,15 +37,14 @@ class Lang_IDE:
 
 # Languages
 langs = [
-    Lang(1, 'Python', '3.11', 29.48, 2), #VSCODE
-    Lang(2, 'C#', '11', 6.94,1), #MVS
-    Lang(3, 'C', 'C17', 6.49,2), #VSCODE
+    Lang(1, 'Python', '3.11', 29.48, 2),  # VSCODE
+    Lang(2, 'C#', '11', 6.94, 1),  # MVS
+    Lang(3, 'C', 'C17', 6.49, 2),  # VSCODE
 
-    Lang(11, 'C++', 'C++20', 6.49,4), #IDEA
-    Lang(22, 'Java', 'Java SE 21', 17.18,5), #Eclipse
-    Lang(33, 'GO', '1.21.3', 36.1,3), #Komodo
+    Lang(11, 'C++', 'C++20', 6.49, 4),  # IDEA
+    Lang(22, 'Java', 'Java SE 21', 17.18, 5),  # Eclipse
+    Lang(33, 'GO', '1.21.3', 36.1, 3),  # Komodo
 ]
-
 
 # Сотрудники
 IDEs = [
@@ -52,24 +55,23 @@ IDEs = [
     IDE(5, 'Eclipse', 2022),
 ]
 
-
 IDEs_langs = [
-    Lang_IDE(1,2), #Py - VSCODE
-    Lang_IDE(2,1), #C# - MVS
-    Lang_IDE(3,2), # C - VSCODE
-    Lang_IDE(1,1), # Py - MVS
-    Lang_IDE(2,2), #C# - VSCODE
+    Lang_IDE(1, 2),  # Py - VSCODE
+    Lang_IDE(2, 1),  # C# - MVS
+    Lang_IDE(3, 2),  # C - VSCODE
+    Lang_IDE(1, 1),  # Py - MVS
+    Lang_IDE(2, 2),  # C# - VSCODE
 
-
-    Lang_IDE(11,4), #C++  - IDEA
-    Lang_IDE(22,5), #Java - Eclipse
-    Lang_IDE(33,3), #GO - Komodo
-    Lang_IDE(11,1), #C++ - MVS
-    Lang_IDE(11,2), #C++ - VSCODE
+    Lang_IDE(11, 4),  # C++  - IDEA
+    Lang_IDE(22, 5),  # Java - Eclipse
+    Lang_IDE(33, 3),  # GO - Komodo
+    Lang_IDE(11, 1),  # C++ - MVS
+    Lang_IDE(11, 2),  # C++ - VSCODE
 ]
 
+
 def solve_a1(one_to_many):
-    res_11 = sorted(one_to_many, key=itemgetter(3)) #Сортировка по названию IDE
+    res_11 = sorted(one_to_many, key=itemgetter(3))  # Сортировка по названию IDE
     return res_11
 
 
@@ -78,15 +80,14 @@ def solve_a2(one_to_many):
     # Перебираем все IDE
     for i in IDEs:
         # Список языков, поддерживаемых средой
-        ide_langs = list(filter(lambda k: k[3]==i.name, one_to_many))
+        ide_langs = list(filter(lambda k: k[3] == i.name, one_to_many))
         # Если хотя бы один язык поддерживается 
         if len(ide_langs) > 0:
             # Доли рынка каждого языка IDE
-            ide_market_shares = [share for _,_,share,_,_ in ide_langs]
+            ide_market_shares = [share for _, _, share, _, _ in ide_langs]
             # Общая доля рынка поддерживаемых языков
             ide_ms_sum = sum(ide_market_shares)
             res_12_unsorted.append((i.name, ide_ms_sum))
-
 
     # Сортировка IDE по суммарной доле рынка
     res_12 = sorted(res_12_unsorted, key=itemgetter(1), reverse=True)
@@ -97,44 +98,57 @@ def solve_a3(many_to_many):
     res_13 = {}
     # Перебираем все IDE
     for i in IDEs:
-        if 'Visual Studio' in i.name: #Если в названии присутствует "Visual Studio"
+        if 'Visual Studio' in i.name:  # Если в названии присутствует "Visual Studio"
             # Список языков IDE
-            i_langs = list(filter(lambda k: k[0]==i.name, many_to_many))
+            i_langs = list(filter(lambda k: k[0] == i.name, many_to_many))
             # Только названия языков 
-            i_langs_names = [x for _,_,x,_,_ in i_langs]
+            i_langs_names = [x for _, _, x, _, _ in i_langs]
             # Добавляем результат в словарь
             # ключ - IDE, значение - список языков
             res_13[i.name] = i_langs_names
 
-
     return res_13
+
+
+one_to_many = []
+many_to_many = []
+
+
+def create_1m() -> list:
+    # Соединение данных один-ко-многим
+    result = [(l.name, l.ver, l.market_share, i.name, i.year)
+              for l in langs
+              for i in IDEs
+              if i.id == l.ide_id]
+    return result
+
+
+def create_mm() -> list:
+    # Соединение данных многие-ко-многим
+    many_to_many_temp = [(i.name, i.year, il.l_id)
+                         for i in IDEs
+                         for il in IDEs_langs
+                         if i.id == il.ide_id]
+
+    result = [(i_name, i_year, l.name, l.ver, l.market_share)
+              for i_name, i_year, l_id in many_to_many_temp
+              for l in langs if l.id == l_id]
+
+    return result
 
 
 def main():
     """Основная функция"""
-    # Соединение данных один-ко-многим 
-    one_to_many = [(l.name, l.ver, l.market_share, i.name, i.year) 
-        for l in langs 
-        for i in IDEs
-        if i.id==l.ide_id]
-    
-    # Соединение данных многие-ко-многим
-    many_to_many_temp = [(i.name, i.year, il.l_id) 
-        for i in IDEs 
-        for il in IDEs_langs 
-        if i.id==il.ide_id]
-    
-    many_to_many = [(i_name, i_year, l.name, l.ver, l.market_share) 
-        for i_name, i_year, l_id in many_to_many_temp
-        for l in langs if l.id==l_id]
-    
+    one_to_many = create_1m()
+    many_to_many = create_mm()
+
     print('Задание А1')
     print(solve_a1(one_to_many))
     print('\nЗадание А2')
     print(solve_a2(one_to_many))
     print('\nЗадание А3')
     print(solve_a3(many_to_many))
-   
+
 
 if __name__ == '__main__':
     main()
--- RK2/tddtest.py
+++ RK2/tddtest.py
@@ -1,12 +1,25 @@
 import unittest
 from main import *
 
-class TestMain(unittest.TestCase):
-    def Test_A1(self):
+one_to_many = create_1m()
+many_to_many = create_mm()
 
-    def Test_A2(self):
-        
-    def Test_A3(self):
+class TestMain(unittest.TestCase):
+    def test_A1(self):
+        self.assertEqual(solve_a1(one_to_many),
+                         [('Java', 'Java SE 21', 17.18, 'Eclipse', 2022), ('C++', 'C++20', 6.49, 'IntelliJ IDEA', 2023),
+                          ('GO', '1.21.3', 36.1, 'Komodo', 2022), ('C#', '11', 6.94, 'Microsoft Visual Studio', 2023),
+                          ('Python', '3.11', 29.48, 'Visual Studio Code', 2023),
+                          ('C', 'C17', 6.49, 'Visual Studio Code', 2023)])
+
+    def test_A2(self):
+        self.assertEqual(solve_a2(one_to_many), [('Komodo', 36.1), ('Visual Studio Code', 35.97), ('Eclipse', 17.18),
+                                                 ('Microsoft Visual Studio', 6.94), ('IntelliJ IDEA', 6.49)])
+
+    def test_A3(self):
+        self.assertEqual(solve_a3(many_to_many), {'Microsoft Visual Studio': ['C#', 'Python', 'C++'],
+                                      'Visual Studio Code': ['Python', 'C', 'C#', 'C++']})
+
 
 if __name__ == "__main__":
-    unittest.main
+    unittest.main()
