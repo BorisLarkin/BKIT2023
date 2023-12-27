748302089f13d3857974bccf07494876f5bf3799
Borizzler <borislarkin18@mail.ru>
2023-12-27 15:10:52 +0300

    Created RK2

---
+++ RK2/tddtest.py
@@ -0,0 +1,12 @@
+import unittest
+from main import *
+
+class TestMain(unittest.TestCase):
+    def Test_A1(self):
+
+    def Test_A2(self):
+        
+    def Test_A3(self):
+
+if __name__ == "__main__":
+    unittest.main
